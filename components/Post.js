9fb308073603c758461d423098651034fef3e527
Cinarrerhan <cinarerhan94@gmail.com>
2022-08-16 21:23:37 +0300

    create the buttons section of the post component

--- components/Post.js
+++ components/Post.js
@@ -1,25 +1,38 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import {
-    DotsHorizontalIcon,
-    HeartIcon,
-    ChatIcon,
-    BookmarkIcon,
-    EmojiHappyIcon,
-  } from "@heroicons/react/outline";
+  DotsHorizontalIcon,
+  HeartIcon,
+  ChatIcon,
+  BookmarkIcon,
+  EmojiHappyIcon,
+} from "@heroicons/react/outline";
 
 function Post({ img, userImg, caption, username, id }) {
   return (
     <div className="bg-white my-7 border rounded-md">
       {/* Post Header */}
       <div className="flex items-center p-5">
-        <img className="h-12 rounded-full object-cover border p-1 mr-3" src={userImg} alt={username} />
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          src={userImg}
+          alt={username}
+        />
         <p className="font-bold flex-1">{username}</p>
         <DotsHorizontalIcon className="h-5" />
       </div>
 
-       {/* Post Image */}
-       <img className="object-cover w-full" src={img} alt={username} />
+      {/* Post Image */}
+      <img className="object-cover w-full" src={img} alt={username} />
+
+      {/* Post Buttons */}
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
     </div>
   );
 }
--- components/Posts.js
+++ components/Posts.js
@@ -7,14 +7,14 @@ function Posts() {
       id: "1",
       username: "erhan",
       userImg: "https://avatars.githubusercontent.com/u/36031081?v=4",
-      img: "https://images.unsplash.com/photo-1660548840757-b92bc9a5f764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+      img: "https://images.unsplash.com/photo-1660590763018-9730ab97a0bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=734&q=80",
       caption: "Thanks for you",
     },
     {
       id: "2",
       username: "cinar",
       userImg: "https://avatars.githubusercontent.com/u/36031081?v=4",
-      img: "https://images.unsplash.com/photo-1611213574952-de754cc6003f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+      img: "https://images.unsplash.com/photo-1660620949725-77cc32219509?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
       caption: "Nice",
     },
   ];
